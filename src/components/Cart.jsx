390138c3e241d17cc6c5cb14dea029c7bbb72d7c
VINAY <vinaytiwari9915@gmail.com>
2023-04-15 11:12:21 +0530

    changes in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -15,30 +15,36 @@ function Cart() {
         cartTotal
       } = useCart();
     
-      if (isEmpty) return <p>Your cart is empty</p>;
+      if (isEmpty) return  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white  flex justify-center ">
+    YOUR CART IS EMPTY
+      </h5>
     
   const [show, setShow] = useState(false);
   return (
     <>
         
-    <div className="bg-gray-100 mr-96 ml-4 font-serif text-2xl underline text-sky-700">
-    <h1>Cart ({totalUniqueItems})</h1>
+    <div className="bg-gray-100 mr-96 ml-4 font-serif text-2xl text-purple-700 opacity-90">
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white  flex justify-center ">
+    NO. OF ITEMS IN YOUR CART :{totalUniqueItems}
+  </h5>
+    
           <ul>
           
             {items.map((item) => (
               <li key={item.id}>
                 <div className="flex justify-between">
-                <img src={item.img} className="mb-5 mt-3 ml-5" style={{width:250, height:200}}/>
-                <p className="pt-10">{item.name} &mdash; Price: {item.price} &nbsp;</p>
+                <img src={item.img} className="mb-5 mt-3 ml-5 border-double border-8 border-gray-900 " style={{width:250, height:200}}/>
+                <p className="pt-10">{item.name}  &nbsp;</p>
+                <p className="pt-10">Price: ₹{item.price} &nbsp;</p>
                <p className="pt-10">Quantity: {item.quantity} </p>
                
                
               
                 
-    <Button  className="bg-slate-200 m-4" onClick={() => updateItemQuantity(item.id, item.quantity - 1)} >             
-      <MdRemoveCircle color="black" size='1.5rem'></MdRemoveCircle></Button>
-      <Button className="bg-slate-200 m-3" onClick={() => updateItemQuantity(item.id, item.quantity + 1)} >             
-      <MdAddCircle color="black" size='1.5rem' ></MdAddCircle></Button>
+    <Button  className="bg-red-600 m-4" onClick={() => updateItemQuantity(item.id, item.quantity - 1)} >             
+      <MdRemoveCircle color="white" size='1.5rem'></MdRemoveCircle></Button>
+      <Button className="bg-green-400 m-3" onClick={() => updateItemQuantity(item.id, item.quantity + 1)} >             
+      <MdAddCircle color="white" size='1.5rem' ></MdAddCircle></Button>
       
              
                 {/* <button   gradientDuoTone="pinkToOrange" onClick={() => removeItem(item.id)}>&times:REMOVE</button> */}
@@ -50,7 +56,8 @@ function Cart() {
   <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white  flex justify-center ">
   TOTAL PRICE: {cartTotal}
   </h5>
-  <div className="m-2 w-2/2  flex justify-center"> <Button  onClick={()=> emptyCart()} > CLEAR CART ITEMS</Button></div>
+  <div className="m-2 w-2/2  flex justify-center"> <Button className="bg-red-400"  onClick={()=> emptyCart()} > CLEAR CART ITEMS</Button>
+  <Button variant="success" className="ml-5 bg-green-400" onClick={()=> emptyCart()}>BUY  </Button></div>
   
 </Card>
            
